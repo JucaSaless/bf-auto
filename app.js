3c1c098d413070d85f4f7bdc1dd44ece2333da36
Jackiel <jucasalesss@gmail.com>
2022-10-17 17:28:26 -0300

    Changes

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -20,6 +25,62 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+
+// Express Session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+//  cookie: { secure: true }
+}));
+
+// Express Validator middleware
+app.use(expressValidator({
+  errorFormatter: function (param, msg, value) {
+      var namespace = param.split('.')
+              , root = namespace.shift()
+              , formParam = root;
+
+      while (namespace.length) {
+          formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+          param: formParam,
+          msg: msg,
+          value: value
+      };
+  },
+  customValidators: {
+      isImage: function (value, filename) {
+          var extension = (path.extname(filename)).toLowerCase();
+          switch (extension) {
+              case '.jpg':
+                  return '.jpg';
+              case '.jpeg':
+                  return '.jpeg';
+              case '.png':
+                  return '.png';
+              case '':
+                  return '.jpg';
+              default:
+                  return false;
+          }
+      }
+  }
+}));
+
+// Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Set routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
