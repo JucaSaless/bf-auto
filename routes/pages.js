1e1f00461203c9fa5e304a4e209899296c2f06b8
Jack_2 <70662997+JucaSaless@users.noreply.github.com>
2022-10-21 21:38:32 -0300

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res) {
         if (err)
             console.log(err);
 
-            res.render('index', {
+            res.render('views/index.ejs', {
                 title: page ? page.title : 'Home_',
                 content: page ? page.content : ''
             });
